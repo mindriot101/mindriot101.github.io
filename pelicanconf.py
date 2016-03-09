bc2831923de302fa7a3b298baf1eabdbea0b5f7a
Simon Walker <s.r.walker101@googlemail.com>
2016-03-09 15:38:45 +0000

    Publish cname on build

--- pelicanconf.py
+++ pelicanconf.py
@@ -45,3 +45,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Copy CNAME file to output
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
