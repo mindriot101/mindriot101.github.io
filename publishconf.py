6b79b78e157cc522652f3b81e025cc892fc6878a
Simon Walker <s.r.walker101@googlemail.com>
2016-05-05 09:15:04 +0100

    Switch to mindriot101.github.io domain

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://blog.simonrw.com'
+SITEURL = 'https://mindriot101.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.xml'
